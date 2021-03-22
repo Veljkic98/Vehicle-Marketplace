bdcee7dea314af5e7ade16333324dbdd388f1c1b
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-03-22 12:36:25 +0100

    Repositories added

---
+++ backend/src/main/java/marketplace/backend/repository/FileRepository.java
@@ -0,0 +1,11 @@
+package marketplace.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import marketplace.backend.model.File;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+    
+}
---
+++ backend/src/main/java/marketplace/backend/repository/ModelRepository.java
@@ -0,0 +1,11 @@
+package marketplace.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import marketplace.backend.model.Model;
+
+@Repository
+public interface ModelRepository extends JpaRepository<Model, Long> {
+    
+}
