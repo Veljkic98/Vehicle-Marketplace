041ab1e5fb9868dd27cbf1d7d915207e0e57ce87
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-11 15:32:55 +0200

    auth user crud controller and service done

---
+++ backend/src/main/java/marketplace/backend/service/AuthenticatedUserService.java
@@ -0,0 +1,67 @@
+package marketplace.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import marketplace.backend.exception.user.EmailIntegrityViolationException;
+import marketplace.backend.exception.user.EmailOrCompanyNameIntegrityViolationException;
+import marketplace.backend.exception.user.UserNotFoundException;
+import marketplace.backend.exception.user.UserOffersConstraintViolationException;
+import marketplace.backend.model.AuthenticatedUser;
+import marketplace.backend.repository.AuthenticatedUserRepository;
+
+@Service
+public class AuthenticatedUserService implements MyService<AuthenticatedUser> {
+
+    @Autowired
+    private AuthenticatedUserRepository authenticatedUserRepository;
+
+    @Override
+    public AuthenticatedUser findById(Long id) {
+
+        AuthenticatedUser user;
+
+        if ((user = authenticatedUserRepository.findById(id).orElse(null)) == null)
+            throw new UserNotFoundException(id);
+
+        return user;
+    }
+
+    @Override
+    public AuthenticatedUser add(AuthenticatedUser entity) {
+
+        try {
+            return authenticatedUserRepository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            throw new EmailOrCompanyNameIntegrityViolationException(entity.getEmail(), entity.getCompanyName());
+        }
+    }
+
+    @Override
+    public AuthenticatedUser update(AuthenticatedUser entity) {
+
+        if (authenticatedUserRepository.findById(entity.getId()).orElse(null) == null)
+            throw new UserNotFoundException(entity.getId());
+
+        try {
+            return authenticatedUserRepository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            throw new EmailOrCompanyNameIntegrityViolationException(entity.getEmail(), entity.getCompanyName());
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        if (authenticatedUserRepository.findById(id).orElse(null) == null)
+            throw new UserNotFoundException(id);
+
+        try {
+            authenticatedUserRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new UserOffersConstraintViolationException(id);
+        }
+
+    }
+}
