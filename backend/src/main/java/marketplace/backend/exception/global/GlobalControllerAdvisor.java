38d2fdb9dc61e58d904454425ccc7141a6bcbe68
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-13 22:37:22 +0200

    global exception controller added

---
+++ backend/src/main/java/marketplace/backend/exception/global/GlobalControllerAdvisor.java
@@ -0,0 +1,30 @@
+package marketplace.backend.exception.global;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import marketplace.backend.exception.Error;
+
+@ControllerAdvice
+public class GlobalControllerAdvisor {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> globalException(Exception e) {
+
+        Error error = new Error(400, "Unhandled error occured.");
+
+        return new ResponseEntity<Error>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Error> dataIntegrityViolation(DataIntegrityViolationException e) {
+
+        Error error = new Error(400, "Data integrity error occured.");
+
+        return new ResponseEntity<Error>(error, HttpStatus.BAD_REQUEST);
+    }
+    
+}
