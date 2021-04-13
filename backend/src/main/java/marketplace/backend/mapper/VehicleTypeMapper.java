70baedabd4d057abb468a32fa783fc3ae1ceb464
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-13 23:46:11 +0200

    vt mapper done

---
+++ backend/src/main/java/marketplace/backend/mapper/VehicleTypeMapper.java
@@ -0,0 +1,36 @@
+package marketplace.backend.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import marketplace.backend.dto.requestDTO.VehicleTypeRequestDTO;
+import marketplace.backend.dto.responseDTO.VehicleTypeResponseDTO;
+import marketplace.backend.model.VehicleType;
+
+public class VehicleTypeMapper implements MyMapper<VehicleType, VehicleTypeResponseDTO, VehicleTypeRequestDTO> {
+
+    @Override
+    public VehicleType toEntity(VehicleTypeRequestDTO dto) {
+
+        return new VehicleType(dto.getName());
+    }
+
+    @Override
+    public VehicleTypeResponseDTO toDto(VehicleType entity) {
+
+        return new VehicleTypeResponseDTO(entity.getId(), entity.getName());
+    }
+
+    @Override
+    public List<VehicleTypeResponseDTO> toDtoList(List<VehicleType> entityList) {
+        
+        List<VehicleTypeResponseDTO> responseDTOs = new ArrayList<>();
+
+        for (VehicleType vehicleType : entityList) {
+            responseDTOs.add(toDto(vehicleType));
+        }
+
+        return responseDTOs;
+    }
+
+}
