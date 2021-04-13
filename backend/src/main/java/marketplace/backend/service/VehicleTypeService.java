67a01b0a14ec6fd52e80c4be1b14ac4d0de916e9
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-13 23:35:37 +0200

    vehicle type service done

---
+++ backend/src/main/java/marketplace/backend/service/VehicleTypeService.java
@@ -0,0 +1,63 @@
+package marketplace.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import marketplace.backend.exception.exceptions.global.MyEntityNotFoundException;
+import marketplace.backend.exception.exceptions.global.UniquenessViolationException;
+import marketplace.backend.exception.exceptions.vehicleType.VehicleTypeNotFoundException;
+import marketplace.backend.model.VehicleType;
+import marketplace.backend.repository.VehicleTypeRepository;
+
+@Service
+public class VehicleTypeService implements MyService<VehicleType> {
+
+    @Autowired
+    private VehicleTypeRepository vehicleTypeRepository;
+
+    @Override
+    public VehicleType findById(Long id) {
+
+        VehicleType type;
+
+        if ((type = vehicleTypeRepository.findById(id).orElse(null)) == null)
+            throw new VehicleTypeNotFoundException(id);
+
+        return type;
+    }
+
+    @Override
+    public VehicleType add(VehicleType entity) {
+
+        try {
+            return vehicleTypeRepository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            throw new UniquenessViolationException("Vehicle type with name: " + entity.getName() + ", already exists.");
+        }
+    }
+
+    @Override
+    public VehicleType update(VehicleType entity) {
+
+        if (vehicleTypeRepository.findById(entity.getId()).orElse(null) == null)
+            throw new VehicleTypeNotFoundException(entity.getId());
+
+        try {
+            return vehicleTypeRepository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            throw new UniquenessViolationException("Vehicle type with name: " + entity.getName() + ", already exists.");
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        if (vehicleTypeRepository.findById(id).orElse(null) == null)
+            throw new VehicleTypeNotFoundException(id);
+
+        vehicleTypeRepository.deleteById(id);
+
+    }
+
+}
