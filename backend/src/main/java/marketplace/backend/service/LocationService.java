66780e21b4ecff9e0ca5d4ab194f05e975975b89
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-04-17 00:58:31 +0200

    Create LocationService.java

---
+++ backend/src/main/java/marketplace/backend/service/LocationService.java
@@ -0,0 +1,63 @@
+package marketplace.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import marketplace.backend.exception.exceptions.global.MyEntityNotFoundException;
+import marketplace.backend.exception.exceptions.global.UniquenessViolationException;
+import marketplace.backend.model.Location;
+import marketplace.backend.repository.LocationRepository;
+
+@Service
+public class LocationService implements MyService<Location> {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Override
+    public Location findById(Long id) {
+
+        Location location;
+
+        if ((location = locationRepository.findById(id).orElse(null)) == null)
+            throw new MyEntityNotFoundException("Locaiton", id);
+
+        return location;
+    }
+
+    @Override
+    public Location add(Location entity) {
+
+        try {
+            return locationRepository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            throw new UniquenessViolationException("Location with city '" + entity.getCity() + "' and country '"
+                    + entity.getCountry() + "' already exists. Or location doesn't exists.");
+        }
+    }
+
+    @Override
+    public Location update(Location entity) {
+
+        if (locationRepository.findById(entity.getId()).orElse(null) == null)
+            throw new MyEntityNotFoundException("Location", entity.getId());
+
+        try {
+            return locationRepository.save(entity);
+        } catch (DataIntegrityViolationException e) {
+            throw new UniquenessViolationException("Location with city '" + entity.getCity() + "' and country '"
+                    + entity.getCountry() + "' already exists. Or location doesn't exists.");
+        }
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        if (locationRepository.findById(id).orElse(null) == null)
+            throw new MyEntityNotFoundException("Location", id);
+
+        locationRepository.deleteById(id);
+    }
+
+}
